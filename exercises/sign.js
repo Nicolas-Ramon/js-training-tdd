6367579ad3c702e944e7551b6c45ea2c97e08bec
Nico-toulouse <nicoramon@hotmail.com>
2020-03-28 20:05:54 +0100

    sign solved

--- exercises/sign.js
+++ exercises/sign.js
@@ -12,6 +12,16 @@
 
 // Your code:
 
+const sign = nber => {
+    if (nber > 0) {
+        return 1;
+    } else if (nber < 0) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
 //* Begin of tests
 const assert = require('assert');
 
