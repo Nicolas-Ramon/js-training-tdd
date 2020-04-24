8bd7d30c9c4ca09bc64a12543660d34473691bee
Nico-toulouse <nicoramon@hotmail.com>
2020-04-24 14:47:39 +0200

    yell solved

--- exercises/yell.js
+++ exercises/yell.js
@@ -8,8 +8,18 @@
 
 // Your code:
 
+function yell(str) {
+    return str.toUpperCase()
+};
+
+
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof yell, 'function');
+assert.strictEqual(yell.length, 1);
+assert.deepStrictEqual(yell('abc'), ('ABC'));
+assert.deepStrictEqual(yell('a b c'), ('A B C'));
+
+// assert.fail('You must write your own tests');
 // End of tests */
