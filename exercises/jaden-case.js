db1b9e9139e0f53c95a8bce9df17a9eb8034a50a
Nico-toulouse <nicoramon@hotmail.com>
2020-04-24 17:32:09 +0200

    jaden-case solved

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -12,8 +12,17 @@
 
 // Your code:
 
+function toJadenCase(str) {
+    return str.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join(' ')
+};
+
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+var str = "How can mirrors be real if our eyes aren't real";
+assert.strictEqual(toJadenCase(str), "How Can Mirrors Be Real If Our Eyes Aren't Real");
+assert.strictEqual(typeof toJadenCase, 'function');
+assert.strictEqual(toJadenCase.length, 1);
+
+// assert.fail('You must write your own tests');
 // End of tests */
