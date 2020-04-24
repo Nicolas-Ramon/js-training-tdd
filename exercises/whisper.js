73a9481c68ae177077d873f42b0fb677d3f53c1b
Nico-toulouse <nicoramon@hotmail.com>
2020-04-24 15:00:07 +0200

    whisper solved

--- exercises/whisper.js
+++ exercises/whisper.js
@@ -9,8 +9,18 @@
 
 // Your code:
 
+function whisper(str) {
+    return '*'+str.toLowerCase()+'*'
+};
+
+
 //* Begin of tests
 const assert = require('assert');
 
-assert.fail('You must write your own tests');
+assert.strictEqual(typeof whisper, 'function');
+assert.strictEqual(whisper.length, 1);
+assert.deepStrictEqual(whisper('ABC'), ('*abc*'));
+assert.deepStrictEqual(whisper('A B C'), ('*a b c*'));
+
+// assert.fail('You must write your own tests');
 // End of tests */
